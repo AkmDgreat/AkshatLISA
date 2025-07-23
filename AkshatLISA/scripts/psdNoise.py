bd89fddc35bed5dedb14c0dfe9b48fac133af4ff
AkmDgreat <admiralakshat@gmail.com>
2025-07-23 16:36:52 -0700

    refactoring (in progress)

--- AkshatLISA/scripts/psdNoise.py
+++ AkshatLISA/scripts/psdNoise.py
@@ -1,5 +1,5 @@
 # from pycbc.noise.gaussian import frequency_noise_from_psd, noise_from_psd
-from scripts.wosa import wosa
+from psd_estimation_methods.wosa import wosa
 import numpy as np
 import scipy.signal
 import random 
@@ -241,76 +241,3 @@ def normalized_psd_residual(orig_psd:  np.ndarray,
         residual = (orig_on_noise - mean_noise) / orig_on_noise
 
     return residual
-
-
-### The following function was a bad idea, cuz what if there is a discontinuity betweeen
-### the two time segments you stich?
-def psdTdiNoiseAveraged(
-    tdi_file_path,
-    channel   = "X",
-    window    = "hann",
-    nperseg      = 4096,
-    noverlap  = None,
-    average   = "mean",
-    scaling   = "density",
-    seed      = None
-):
-    """
-    Computes:
-        psd_true   – PSD of the original TDI data (WOSA)
-        freqs      – frequency vector
-        psd_noise  – PSD of a synthetic noise record that has the
-                     same length and therefore the same # of Welch
-                     averages as psd_true
-    """
-    # ------------------------------------------------------------------
-    # 1) PSD of original TDI time-series
-    # ------------------------------------------------------------------
-    obs   = TimeSeriesDict.read(tdi_file_path)
-    data  = obs[channel].value
-    dt    = obs[channel].dt.value
-    fs    = 1.0 / dt
-
-    freqs, psd_true, _, _ = wosa(
-        x          = data,
-        fs         = fs,
-        window     = window,
-        nperseg    = nper,
-        noverlap   = noverlap,
-        scaling    = scaling,
-        average    = average,
-    )
-
-    n_samples_target = len(data)           # how long our noise must be
-    one_seg_psd      = psd_true            # same spectral shape
-    noise_segments   = []
-
-    # ------------------------------------------------------------------
-    # 2) Stitch together enough segments
-    # ------------------------------------------------------------------
-    seg_idx = 0
-    while sum(len(seg) for seg in noise_segments) < n_samples_target:
-        noise_segments.append(
-            _noise_segment_from_psd(
-                one_seg_psd, fs, nper,
-                seed = None if seed is None else seed + seg_idx
-            )
-        )
-        seg_idx += 1
-
-    time_noise = np.concatenate(noise_segments)[:n_samples_target]
-
-    # ------------------------------------------------------------------
-    # 3) PSD of the synthetic noise record (same params as true PSD)
-    # ------------------------------------------------------------------
-    _, psd_noise, _, _ = wosa(
-        x          = time_noise,
-        fs         = fs,
-        window     = window,
-        nperseg    = nper,
-        noverlap   = noverlap,
-        scaling    = scaling,
-        average    = average,
-    )
-
-    return psd_true, freqs, psd_noise
